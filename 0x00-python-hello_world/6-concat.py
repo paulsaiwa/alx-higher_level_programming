10015d7e78532ea720eb0ed3e2c179d2758a4b62
paulsaiwa <saiwavincent@gmail.com>
2022-09-19 13:05:36 +0000

    6-concat.py

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-str = "Welcome to Holberton School!"
-print(("{}\n{}").format(str*3, str[:9]))
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + ' ' + str2
+print("Welcome to {}!".format(str1))
