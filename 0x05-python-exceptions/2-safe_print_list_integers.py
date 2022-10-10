c12bbb1461f96d781feae04231e86c27f28b220c
paulsaiwa <saiwavincent@gmail.com>
2022-10-10 04:35:45 -0700

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-
-
 def safe_print_list_integers(my_list=[], x=0):
-    """
-    prints a list of anything, but only prints the integers
-    Returns the amount of integers printed
-    """
-    i = 0
-    printed = 0
-    for i in range(0, x):
+    c = 0
+    for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            printed += 1
+            c += 1
         except (ValueError, TypeError):
             continue
     print()
-    return printed
+    return c
