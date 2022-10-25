44a38be3996c1ed8861228132fd12c64894277af
paulsaiwa <saiwavincent@gmail.com>
2022-10-25 17:59:58 +0000

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""
+Contains the "class_to_json" function
+"""
 
 
 def class_to_json(obj):
-    return vars(obj)
+    """returns the dictionary description with simple data structure
+    """
+    return obj.__dict__
