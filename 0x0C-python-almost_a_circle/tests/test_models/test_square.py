1cdc974b009d0cacf1d058888eb402bbb95fb219
paulsaiwa <saiwavincent@gmail.com>
2022-11-01 08:35:42 +0000

    test_square.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,128 +1,194 @@
 #!/usr/bin/python3
-"""Unittest for class Square
-"""
+"""Unittest cases por square.py"""
 import unittest
-import os
-from io import StringIO
-from unittest.mock import patch
-from models.base import Base
+import pep8
 from models.rectangle import Rectangle
 from models.square import Square
+from models.base import Base
+from contextlib import redirect_stdout as out
+import io
+import os
 
 
 class TestSquare(unittest.TestCase):
-    """Testing Square
     """
+    Args:
+        unittest
+    """
+    pass
 
-    def tearDown(self):
-        """Tears down obj count
-        """
-
+    def setUp(self):
+        """ print('setUp') """
         Base._Base__nb_objects = 0
         self.assertEqual(Base._Base__nb_objects, 0)
 
-    def test_instance(self):
-        """Test instantiation
-        """
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/square.py'])
+        self.assertEqual(
+            result.total_errors, 0, "Found code style errors (and warnings).")
 
-        o1 = Square(5)
-        o2 = Square(id="hello", size=3)
+    def test_sqr_id(self):
+        """OK"""
+        sq = Square(5)
+        self.assertEqual(sq.id, 1)
+        sq = Square(2, 1)
+        self.assertEqual(sq.id, 2)
+        sq = Square(10, 0, 0, 55)
+        self.assertEqual(sq.id, 55)
+
+    def test_subcls(self):
+        """sub class"""
+        sq = Square(10, 2)
+        self.assertTrue(issubclass(Square, Base))
+        self.assertTrue(issubclass(Square, Rectangle))
+
+    def test_width(self):
+        """sq width"""
+        sq = Square(5)
+        self.assertEqual(sq.width, 5)
+
+    def test_size(self):
+        """sq size"""
+        sq = Square(5)
+        self.assertEqual(sq.size, 5)
+
+    def test_err_size(self):
+        """size 0"""
         with self.assertRaises(ValueError):
-            o3 = Square(-5, 3, 4)
-            o4 = Square(9.5, 9.3)
-            o5 = Square(float('inf'))
-            o6 = Square("string")
-            o9 = Square(None)
+            sq = Square(0)
 
+    def test_x(self):
+        """x"""
+        sq = Square(10, 2)
+        self.assertEqual(sq.x, 2)
+        sq = Square(10, 3, 2, 5)
+        self.assertEqual(sq.x, 3)
+        sq = Square(3, 1, 0)
+        self.assertEqual(sq.x, 1)
+
+    def test_x_neg(self):
+        """negative x"""
+        with self.assertRaises(ValueError):
+            r = Rectangle(2, -1)
+
+    def test_x_str(self):
+        """str as x"""
         with self.assertRaises(TypeError):
-            o7 = Square(5, "hi")
-            o8 = Square(5, None)
-            o10 = Square(5, float('inf'))
-            o11 = Square(5, 9.5)
-            o12 = Square()
+            r = Rectangle(2, '')
 
-        self.assertEqual(o1.id, 1)
-        self.assertEqual(o1._Base__nb_objects, 3)
-        self.assertEqual(o2.id, 'hello')
-        self.assertEqual(o2._Base__nb_objects, 3)
+    def test_x_dict(self):
+        """dict as x"""
+        with self.assertRaises(TypeError):
+            r = Rectangle(2, {})
 
-    def test_area(self):
-        """Testing area()
-        """
+    def test_x_tup(self):
+        """tuple as x"""
+        with self.assertRaises(TypeError):
+            r = Rectangle(2, ())
 
-        o1 = Square(5)
-        o2 = Square(999, 0, 0, "helloo")
-        o3 = Square(id="hello", size=3, x=1, y=0)
+    def test_x_list(self):
+        """list as x"""
+        with self.assertRaises(TypeError):
+            r = Rectangle(2, [])
 
-        self.assertEqual(o1.area(), 25)
-        self.assertEqual(o2.area(), 998001)
-        self.assertEqual(o3.area(), 9)
+    def test_x_float(self):
+        """float as x"""
+        with self.assertRaises(TypeError):
+            r = Rectangle(2, 3.14159)
 
-    def test_display(self):
-        """Testing display()
-        """
+    def test_x_none(self):
+        """none as x"""
+        with self.assertRaises(TypeError):
+            r = Rectangle(2, None)
 
-        o1 = Square(4)
-        o2 = Square(id="hello", size=3, x=1, y=0)
+    def test_x_set(self):
+        """set as x"""
+        with self.assertRaises(TypeError):
+            r = Rectangle(2, {1})
 
-        with patch('sys.stdout', new=StringIO()) as fakeOutput:
-            o1.display()
-            self.assertEqual(fakeOutput.getvalue(), '####\n####\n####\n####\n')
-
-        with patch('sys.stdout', new=StringIO()) as fakeOutput:
-            o2.display()
-            self.assertEqual(fakeOutput.getvalue(), ' ###\n ###\n ###\n')
-
-    def test_str(self):
-        """Testing __str__()
-        """
-
-        o1 = Square(5)
-        o2 = Square(3, 2)
-        o3 = Square(1, 2, 3, 4)
-        o4 = Square(id="hello", size=3, x=1, y=0)
-
-        self.assertEqual(o1.__str__(), '[Square] (1) 0/0 - 5')
-        self.assertEqual(o2.__str__(), '[Square] (2) 2/0 - 3')
-        self.assertEqual(o3.__str__(), '[Square] (4) 2/3 - 1')
-        self.assertEqual(o4.__str__(), '[Square] (hello) 1/0 - 3')
-
-    def test_update(self):
-        """Testing update()
-        """
-
-        o1 = Square(5)
-        o2 = Square(3, 2)
-        o3 = Square(1, 2, 3, 4)
-        o4 = Square(id="hello", size=3, x=1, y=0)
-
-        o1.update(6, 1, 2, 8)
-        self.assertEqual(o1.__str__(), '[Square] (6) 2/8 - 1')
-        o2.update(1, 2, 3, id="hello")
-        self.assertEqual(o2.__str__(), '[Square] (hello) 2/0 - 3')
+    def test_w_neg(self):
+        """negative width"""
         with self.assertRaises(ValueError):
-            o3.update("hello", -5)
-            o4.update(x=9.5)
+            r = Square(-2)
 
-    def test_to_dictionary(self):
-        """Testing to_dictionary()
-        """
+    def test_w_str(self):
+        """str as width"""
+        with self.assertRaises(TypeError):
+            r = Square('')
 
-        o1 = Square(5)
-        o2 = Square(5, 6)
-        o3 = Square(1, 2, 3, 5)
-        o4 = Square(3, 2, id="holberton")
+    def test_w_dict(self):
+        """dict as width"""
+        with self.assertRaises(TypeError):
+            r = Square({})
 
-        d1 = {'id': 1, 'size': 5, 'x': 0, 'y': 0}
-        d2 = {'id': 2, 'size': 5, 'x': 6, 'y': 0}
-        d3 = {'id': 5, 'size': 1, 'x': 2, 'y': 3}
-        d4 = {'id': 'holberton', 'size': 3, 'x': 2, 'y': 0}
+    def test_w_tup(self):
+        """tuple as width"""
+        with self.assertRaises(TypeError):
+            r = Square(())
 
-        self.assertDictEqual(o1.to_dictionary(), d1)
-        self.assertDictEqual(o2.to_dictionary(), d2)
-        self.assertDictEqual(o3.to_dictionary(), d3)
-        self.assertDictEqual(o4.to_dictionary(), d4)
+    def test_w_list(self):
+        """list as width"""
+        with self.assertRaises(TypeError):
+            r = Square([])
 
-if __name__ == '__main__':
+    def test_w_float(self):
+        """float as width"""
+        with self.assertRaises(TypeError):
+            r = Rectangle(3.14159)
+
+    def test_w_none(self):
+        """none as width"""
+        with self.assertRaises(TypeError):
+            r = Rectangle(None)
+
+    def test_w_set(self):
+        """set as width"""
+        with self.assertRaises(TypeError):
+            r = Rectangle({1})
+
+    def test_y_neg(self):
+        """negative y"""
+        with self.assertRaises(ValueError):
+            r = Rectangle(2, 1, -1)
+
+    def test_y_str(self):
+        """str as y"""
+        with self.assertRaises(TypeError):
+            r = Rectangle(10, 1, '')
+
+    def test_y_dict(self):
+        """dict as y"""
+        with self.assertRaises(TypeError):
+            r = Rectangle(10, 2, {})
+
+    def test_y_tup(self):
+        """tuple as y"""
+        with self.assertRaises(TypeError):
+            r = Rectangle(10, 2, ())
+
+    def test_y_list(self):
+        """list as y"""
+        with self.assertRaises(TypeError):
+            r = Rectangle(10, 2, [])
+
+    def test_y_float(self):
+        """float as y"""
+        with self.assertRaises(TypeError):
+            r = Rectangle(10, 2, 3.14159)
+
+    def test_y_none(self):
+        """none as y"""
+        with self.assertRaises(TypeError):
+            r = Rectangle(10, 2, None)
+
+    def test_y_set(self):
+        """set as y"""
+        with self.assertRaises(TypeError):
+            r = Rectangle(2, 10, {1})
+
+
+if __name__ == "__main__":
+    """main"""
     unittest.main()
-
